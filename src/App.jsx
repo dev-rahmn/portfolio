fc69464ad8bd2d9f8d33b72ccb19734439fee6b0
dev-rahmn <ar867301@gmail.com>
2025-03-10 14:43:21 +0530

    path fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-import NavbarMain from './components/navbar/NavBarMain'
+import NavbarMain from './components/navbar/NavbarMain'
 import HeroMain from './components/heroSection/HeroMain'
 import HeroGradient from './components/heroSection/HeroGradient'
 import SubHeroSection from './components/heroSection/SubHeroSection'
